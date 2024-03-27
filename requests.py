bad25f717cae84de94d92211d7c284eaa1727516
shreeangane <165073488+shreeangane@users.noreply.github.com>
2024-03-27 17:34:42 +0530

    Update requests.py

--- requests.py
+++ requests.py
@@ -1,3 +1,5 @@
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+# st.text(fruityvice_response.json())
+
+fv_dt = st.dataframe(date=fruityvice_response.json(), use_container_width=True)
