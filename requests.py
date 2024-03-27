708556c7c5206df6aec2a9885b3785e8e5eca869
shreeangane <165073488+shreeangane@users.noreply.github.com>
2024-03-27 17:45:44 +0530

    Delete requests.py

--- requests.py
+++
@@ -1,5 +0,0 @@
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-
-fv_dt = st.dataframe(date=fruityvice_response.json(), use_container_width=True)
